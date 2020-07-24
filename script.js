37fc1a79101d65c3ec48a84e5bad9e19255aadb0
puen <surayut.bo@ku.th>
2020-07-24 23:05:52 +0700

    add the last favourite movie, update layout for mobile, change font

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-function switchTitle(e) {
-    poster.src = content[e].src
-    head.innerHTML = content[e].head
-    title.innerHTML = content[e].title
-    des.innerHTML = content[e].des
-}
-
 var content
 fetch('data.json')
     .then(response => response.json())
@@ -12,12 +5,9 @@ fetch('data.json')
         content = data
     });
 
-let head = document.getElementById('head')
-let title = document.getElementById('title')
-let des = document.getElementById('des')
-let poster = document.getElementById('poster')
-
-head.innerHTML = "THE FIRST"
-title.innerHTML = "Music and Lyrics"
-poster.src = "https://upload.wikimedia.org/wikipedia/en/d/d3/Music_and_lyrics.jpg"
-des.innerHTML = "I actually never thought to see this kind of movie before, but this movie is the first romatic-comedy movie for me and made me want to watch more this kind of movie. I always feel good when I watch this kind of movie. In addition, the original sound tracks are so good. it made me want to watch this again."
+function switchTitle(e) {
+    document.getElementById('head').innerHTML = content[e].head
+    document.getElementById('title').innerHTML = content[e].title
+    document.getElementById('des').innerHTML = content[e].des
+    document.getElementById('poster').src = content[e].src
+}
